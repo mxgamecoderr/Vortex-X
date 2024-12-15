fa9b9fa712973a727f6b6fe01375b53fee6448ac
mxgamecoder <OlamilekanDamilaraa@gmail.com>
2024-12-15 14:12:46 +0100

    Add files via upload

---
+++ Cmds/Owner/save.js
@@ -0,0 +1,13 @@
+/*   mxgamecoder :v
+
+What's The Point Of This Code ? */
+
+const ownerMiddleware = require('../../utility/botUtil/Ownermiddleware'); 
+
+module.exports = async (context) => {
+    await ownerMiddleware(context, async () => {
+        const { client, m, Owner } = context;
+
+})
+
+}
