aee7384ec494a88242d70bb50eb3aaeb34f3b247
mxgamecoder <OlamilekanDamilaraa@gmail.com>
2024-12-15 11:03:57 +0100

    Add files via upload

---
+++ utility/botUtil/middleware.js
@@ -0,0 +1,19 @@
+
+
+const middleware = async (context, next) => {
+    const { m, isBotAdmin, isAdmin } = context;
+
+    if (!m.isGroup) {
+        return m.reply("This command is meant for groups only please 🥺");
+    }
+    if (!isAdmin) {
+        return m.reply("You need admin privileges 🥺");
+    }
+    if (!isBotAdmin) {
+        return m.reply("I need admin privileges 🥺");
+    }
+
+    await next(); // Proceed to the next function (main handler)
+};
+
+module.exports = middleware;
